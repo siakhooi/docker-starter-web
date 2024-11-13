62072a9c3c23fddebf8f0a05b2282e7e551e06b9
Siak Hooi <siakhooi@gmail.com>
2024-11-13 10:35:19 +0000

    bash script with google style

--- .github/workflows/build-script.sh
+++ .github/workflows/build-script.sh
@@ -1,9 +1,14 @@
-cd $MY_DIRECTORY
+#!/bin/bash
 
-docker build -f $MY_DOCKERFILE -t $MY_REGISTRY/$MY_TAG  .
+cd "$MY_DIRECTORY" || {
+  echo "Directory not found"
+  exit 1
+}
+
+docker build -f "$MY_DOCKERFILE" -t "$MY_REGISTRY/$MY_TAG" .
 
 docker images
 
-docker login -u $MY_REGISTRY_USER -p "$MY_REGISTRY_PASSWORD" $MY_REGISTRY
+docker login -u "$MY_REGISTRY_USER" -p "$MY_REGISTRY_PASSWORD" "$MY_REGISTRY"
 
-docker push $MY_REGISTRY/$MY_TAG
+docker push "$MY_REGISTRY/$MY_TAG"
